3b89cd6b26bdc6ce88cfd811c678f1f37c964072
kumarpawa <kumarpawa@myvuw.ac.nz>
2021-11-12 12:10:16 +1300

    Test

--- App.js
+++ App.js
@@ -1,22 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { Input } from 'react-native-elements';
 
-export default function App() {
+const GOOGLE_PLACES_API_KEY = 'AIzaSyBVASTnFE1PZfRclI49MXAEBYsf62fH5SU';
+
+const GooglePlacesInput = () => {
   return (
-    <View style={styles.container}>
-      <Text>Magic Meal app!</Text>
-      <Text>Get your food now!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GooglePlacesAutocomplete
+      query={{
+        key: GOOGLE_PLACES_API_KEY,
+        language: 'en', // language of the results
+      }}
+      onPress={(data, details) => console.log(data, details)}
+      textInputProps={{
+        InputComp: Input,
+        leftIcon: { type: 'font-awesome', name: 'chevron-left' },
+        errorStyle: { color: 'red' },
+      }}
+    />
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default GooglePlacesInput;
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Magic Meal app!</Text>
+//       <Text>Get your food now!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
